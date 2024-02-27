f677c49a96b532143e6d05cf27b1ffb09f8b17c4
MarioPasc <mario.pg02@gmail.com>
2024-02-27 16:05:08 +0100

    Tests método basalMetabolicRate

--- src/test/java/healthcalc/HealthCalcTest.java
+++ src/test/java/healthcalc/HealthCalcTest.java
@@ -12,35 +12,136 @@ public class HealthCalcTest {
 	private final HealthCalcImpl healthCalc = new HealthCalcImpl();
 	
 	// Tests para el método "idealWeight" de la clase HealthCalcImpl
+		
+	// Tests para valores de entrada negativos o igual a cero
+
 	@Test
-	@DisplayName("Test altura negativa")
-	public void testAlturaNegativa() {
+	@DisplayName("Test altura negativa para idealWeight")
+	public void testAlturaNegativaIdealWeight() {
 		assertThrows(IllegalArgumentException.class, 
 		() -> healthCalc.idealWeight(-5, 'm'), 
 		"La altura debe ser un número positivo.");
 	}
 
 	@Test
-	@DisplayName("Test altura cero")
-	public void testAlturaCero() {
+	@DisplayName("Test altura cero para idealWeight")
+	public void testAlturaCeroIdealWeight() {
 		assertThrows(IllegalArgumentException.class, 
 		() -> healthCalc.idealWeight(0, 'm'), 
 		"La altura debe ser un número positivo.");
 	}
 
+	// Tests para valores que desbordan el tipo de datos
+
 	@Test
-	@DisplayName("Test altura overflow")
-	public void testAlturaOverflow() {
+	@DisplayName("Test altura overflow para idealWeight")
+	public void testAlturaOverflowIdealWeight() {
 		assertThrows(IllegalArgumentException.class, 
 		() -> healthCalc.idealWeight(Integer.MAX_VALUE, 'm'), 
 		"La altura proporcionada es demasiado grande.");
 	}
 
+	// Otros tests de parámetros de entrada no válidos
+
 	@Test
-	@DisplayName("Test género no válido")
-	public void testGeneroNoValido() {
+	@DisplayName("Test género no válido para idealWeight")
+	public void testGeneroNoValidoIdealWeight() {
 		assertThrows(IllegalArgumentException.class, 
 		() -> healthCalc.idealWeight(100, 'x'), 
 		"El género debe ser 'm' (hombre) o 'w' (mujer).");
 	}
+
+	// Tests para el método "basalMetabolicRate" de la clase HealthCalcImpl
+
+	// Tests relacionados con parámetros negativos o igual a cero
+
+	@Test
+	@DisplayName("Test altura negativa basalMetabolicRate")
+	public void testAlturaNegativaBasalMetabolicRate() {
+		assertThrows(IllegalArgumentException.class, 
+		() -> healthCalc.basalMetabolicRate(60, -1, 'm', 20), 
+		"La altura debe ser un número positivo.");
+	}
+
+	@Test
+	@DisplayName("Test altura cero basalMetabolicRate")
+	public void testAlturaCeroBasalMetabolicRate() {
+		assertThrows(IllegalArgumentException.class, 
+		() -> healthCalc.basalMetabolicRate(60, 0, 'm', 20), 
+		"La altura debe ser un número positivo.");
+	}
+
+	@Test
+	@DisplayName("Test peso negativo basalMetabolicRate")
+	public void testPesoNegativoBasalMetabolicRate() {
+		assertThrows(IllegalArgumentException.class, 
+			() -> healthCalc.basalMetabolicRate(-1, 170, 'm', 20), 
+			"El peso debe ser un número positivo.");
+	}
+
+	@Test
+	@DisplayName("Test edad negativa basalMetabolicRate")
+	public void testEdadNegativaBasalMetabolicRate() {
+		assertThrows(IllegalArgumentException.class, 
+			() -> healthCalc.basalMetabolicRate(60, 170, 'm', -1), 
+			"La edad debe ser un número positivo.");
+	}
+
+	@Test
+	@DisplayName("Test peso cero basalMetabolicRate")
+	public void testPesoCeroBasalMetabolicRate() {
+		assertThrows(IllegalArgumentException.class, 
+			() -> healthCalc.basalMetabolicRate(0, 170, 'm', 20), 
+			"El peso debe ser un número positivo.");
+	}
+
+	@Test
+	@DisplayName("Test edad cero basalMetabolicRate")
+	public void testEdadCeroBasalMetabolicRate() {
+		assertThrows(IllegalArgumentException.class, 
+			() -> healthCalc.basalMetabolicRate(60, 170, 'm', 0), 
+			"La edad debe ser un número positivo.");
+	}
+
+	// Tests para valores de entrada que desbordan el tipo de datos
+
+	@Test
+	@DisplayName("Test altura overflow para basalMetabolicRate")
+	public void testAlturaOverflowBasalMetabolicRate() {
+		assertThrows(IllegalArgumentException.class, 
+		() -> healthCalc.basalMetabolicRate(60, Integer.MAX_VALUE, 'm', 20), 
+		"La altura proporcionada es demasiado grande.");
+	}
+
+	@Test
+	@DisplayName("Test peso overflow para basalMetabolicRate")
+	public void testPesoOverflowBasalMetabolicRate() {
+		assertThrows(IllegalArgumentException.class, 
+			() -> healthCalc.basalMetabolicRate(Integer.MAX_VALUE, 170, 'm', 20), 
+			"El peso proporcionado es demasiado grande.");
+	}
+
+	@Test
+	@DisplayName("Test edad overflow para basalMetabolicRate")
+	public void testEdadOverflowBasalMetabolicRate() {
+		assertThrows(IllegalArgumentException.class, 
+			() -> healthCalc.basalMetabolicRate(60, 170, 'm', Integer.MAX_VALUE), 
+			"La edad proporcionada es demasiado grande.");
+	}
+
+	// Otros tests de parámetros de entrada no válidos
+
+	@Test
+	@DisplayName("Test género no válido para basalMetabolicRate")
+	public void testGeneroNoValidoBasalMetabolicRate() {
+		assertThrows(IllegalArgumentException.class, 
+		() -> healthCalc.basalMetabolicRate(60, 170, 'x', 0), 
+		"El género debe ser 'm' (hombre) o 'w' (mujer).");
+	}
+
+
+
+
+
+	
 }
