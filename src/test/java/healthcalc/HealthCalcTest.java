1169f482c67813f94d0f09c5cc7075bc614ad199
MarioPasc <mario.pg02@gmail.com>
2024-02-27 15:36:41 +0100

    Tests método idealWeight

--- src/test/java/healthcalc/HealthCalcTest.java
+++ src/test/java/healthcalc/HealthCalcTest.java
@@ -1,6 +1,7 @@
 package healthcalc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,9 +9,38 @@ import org.junit.jupiter.api.Test;
 @DisplayName("Tests para la calculadora de salud.")
 public class HealthCalcTest {
 
+	private final HealthCalcImpl healthCalc = new HealthCalcImpl();
+	
+	// Tests para el método "idealWeight" de la clase HealthCalcImpl
 	@Test
-	@DisplayName("Esto es un test de ejemplo.")
-	public void bmi() {
-		assertEquals(true, true);
+	@DisplayName("Test altura negativa")
+	public void testAlturaNegativa() {
+		assertThrows(IllegalArgumentException.class, 
+		() -> healthCalc.idealWeight(-5, 'm'), 
+		"La altura debe ser un número positivo.");
+	}
+
+	@Test
+	@DisplayName("Test altura cero")
+	public void testAlturaCero() {
+		assertThrows(IllegalArgumentException.class, 
+		() -> healthCalc.idealWeight(0, 'm'), 
+		"La altura debe ser un número positivo.");
+	}
+
+	@Test
+	@DisplayName("Test altura overflow")
+	public void testAlturaOverflow() {
+		assertThrows(IllegalArgumentException.class, 
+		() -> healthCalc.idealWeight(Integer.MAX_VALUE, 'm'), 
+		"La altura proporcionada es demasiado grande.");
+	}
+
+	@Test
+	@DisplayName("Test género no válido")
+	public void testGeneroNoValido() {
+		assertThrows(IllegalArgumentException.class, 
+		() -> healthCalc.idealWeight(100, 'x'), 
+		"El género debe ser 'm' (hombre) o 'w' (mujer).");
 	}
 }
